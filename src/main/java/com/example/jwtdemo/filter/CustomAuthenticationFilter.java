24d38af4add6cab649dffbb58f01f0c8d05a5cdf
zhangr4 <ran.zhang863@hotmail.com>
2021-08-08 12:30:39 +0800

    complete the tutorial

--- src/main/java/com/example/jwtdemo/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/example/jwtdemo/filter/CustomAuthenticationFilter.java
@@ -67,14 +67,14 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         here is the authority of different roles  */
         String access_token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis()*10*60*1000))
+                .withExpiresAt(new Date(System.currentTimeMillis()+1*60*1000))
                 .withIssuer(request.getRequestURL().toString())
                 .withClaim("roles",user.getAuthorities().stream().map(
                         GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(algorithm);
         String refresh_token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis()*30*60*1000))
+                .withExpiresAt(new Date(System.currentTimeMillis()+10*60*1000))
                 .withIssuer(request.getRequestURL().toString())
                 .sign(algorithm);
 
--- src/main/java/com/example/jwtdemo/security/SecurityConfig.java
+++ src/main/java/com/example/jwtdemo/security/SecurityConfig.java
@@ -1,17 +1,20 @@
 package com.example.jwtdemo.security;
 
 import com.example.jwtdemo.filter.CustomAuthenticationFilter;
+import com.example.jwtdemo.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
@@ -49,11 +52,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
         //allow any request to "/login", it is a default
-        http.authorizeRequests().antMatchers("/api/login/**").permitAll();
+        http.authorizeRequests().antMatchers("/api/login/**","/api/refreshtoken/**").permitAll();
         http.authorizeRequests().antMatchers(GET,"/api/user/**").hasAnyRole("ROLE_USER");
         http.authorizeRequests().antMatchers(POST,"/api/user/save/**").hasAnyRole("ROLE_ADMIN");
+        http.authorizeRequests().anyRequest().authenticated();
         //add filter for authentication
         http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 
 
 
